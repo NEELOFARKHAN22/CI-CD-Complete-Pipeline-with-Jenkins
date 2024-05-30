6d27e6da7fa1433a4106b69e0b011d8d1fc29cb5
NEELOFARKHAN22 <138312598+NEELOFARKHAN22@users.noreply.github.com>
2024-05-30 12:30:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,35 @@
+# Creating key-pair on AWS using SSH-public key
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key-name
+  public_key = file("${path.module}/my-key.pub")
+}
 
+# Creating a security group to restrict/allow inbound connectivity
+resource "aws_security_group" "network-security-group" {
+  name        = var.network-security-group-name
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  # Not recommended to add "0.0.0.0/0" instead we need to be more specific with the IP ranges to allow connectivity from.
+  tags = {
+    Name = "nsg-inbound"
+  }
+}
+
+
+# Creating Ubuntu EC2 instance
+resource "aws_instance" "ubuntu-vm-instance" {
+  ami             = var.ubuntu-ami
+  instance_type   = var.ubuntu-instance-type
+  key_name        = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.network-security-group.id]
+  tags = {
+    Name = "ubuntu-vm"
+  }
+}
